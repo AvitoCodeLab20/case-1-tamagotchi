4aa1921201dc0ee61de2826aef023d278e5bf077
Nikosweet <kupchikov1975@mail.ru>
2026-08-04 15:48:20 +0700

    feat(client): configure react-query

--- frontend/client/src/app/app/App.tsx
+++ frontend/client/src/app/app/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import { QueryClientProvider } from '@app/providers/queryClient';
 
 export function App() {
     return (
-        <>
+        <QueryClientProvider>
             <Outlet />
-        </>
+        </QueryClientProvider>
     );
 }
